5385d5b8aeac90307931da6653aa5e4a694d889e
FadliArifRamadan15 <fadli.ramadhan.ti.20@cic.ac.id>
2022-11-24 11:53:57 +0700

    ganti judul

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -10,12 +10,12 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
+        title: const Text('Home'),
         centerTitle: true,
       ),
       body: Center(
         child: Text(
-          'HomeView is working',
+          'Home is working',
           style: TextStyle(fontSize: 20),
         ),
       ),
