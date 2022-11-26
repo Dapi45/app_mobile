fdcf56745c0eb3c7bc7a117a37ad68ea3d2cc913
Dapi45 <daffifadil45@gmail.com>
2022-11-27 00:06:02 +0700

    Bottom Nav

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
-import '../controllers/home_controller.dart';
+import '../controllers/login_controller.dart';
 
-class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
+class LoginView extends GetView<LoginController> {
+  const LoginView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Dashboard'),
+        title: const Text('LoginView'),
         centerTitle: true,
       ),
       body: Center(
         child: Text(
-          'Halaman Dashboard',
+          'LoginView is working',
           style: TextStyle(fontSize: 20),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:app_mobile/app/modules/login/views/login_view.dart';
+import 'package:fancy_bottom_navigation_2/fancy_bottom_navigation.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
@@ -8,8 +9,135 @@ void main() {
   runApp(
     GetMaterialApp(
       title: "Application",
-      initialRoute: AppPages.INITIAL,
+      debugShowCheckedModeBanner: false,
+      // initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      theme: ThemeData.dark(),
+      home: MyHomePage(),
     ),
   );
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentPage = 0;
+
+  GlobalKey bottomNavigationKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("App Name"),
+      ),
+      body: Container(
+        // decoration: BoxDecoration(color: Colors.white),
+        child: Center(
+          child: _getPage(currentPage),
+        ),
+      ),
+      bottomNavigationBar: FancyBottomNavigation(
+        tabs: [
+          TabData(
+              iconData: Icons.home,
+              title: "Home",
+              onclick: () {
+                final FancyBottomNavigationState fState = bottomNavigationKey
+                    .currentState as FancyBottomNavigationState;
+                fState.setPage(2);
+              }),
+          TabData(
+              iconData: Icons.home_repair_service,
+              title: "Service",
+              onclick: () => Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => LoginView()))),
+          TabData(iconData: Icons.person, title: "Profile"),
+        ],
+        initialSelection: 0,
+        key: bottomNavigationKey,
+        onTabChangedListener: (position) {
+          setState(() {
+            currentPage = position;
+          });
+        },
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[Text("Cek"), Text("1 2 3")],
+        ),
+      ),
+    );
+  }
+
+  _getPage(int page) {
+    switch (page) {
+      case 0:
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("Tes"),
+            RaisedButton(
+              child: Text(
+                "Start new page",
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Theme.of(context).primaryColor,
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => LoginView()));
+              },
+            ),
+            RaisedButton(
+              child: Text(
+                "Change to page 3",
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Theme.of(context).primaryColor,
+              onPressed: () {
+                final FancyBottomNavigationState fState = bottomNavigationKey
+                    .currentState as FancyBottomNavigationState;
+                fState.setPage(2);
+              },
+            )
+          ],
+        );
+      case 1:
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("Tes"),
+            RaisedButton(
+              child: Text(
+                "Start new page",
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Theme.of(context).primaryColor,
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => LoginView()));
+              },
+            )
+          ],
+        );
+      default:
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("Tes"),
+            RaisedButton(
+              child: Text(
+                "Start new page",
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Theme.of(context).primaryColor,
+              onPressed: () {},
+            )
+          ],
+        );
+    }
+  }
 }
