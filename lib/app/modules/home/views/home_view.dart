1bc20a1c7dea36ce6f378027fdb19b6eff6e7c6e
Dapi45 <daffifadil45@gmail.com>
2022-12-01 15:36:01 +0700

    home

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,22 +1,68 @@
+import 'package:fancy_bottom_navigation_2/fancy_bottom_navigation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../routes/app_pages.dart';
+import '../../../widget/navbot.dart';
 import '../controllers/home_controller.dart';
+
+int currentPage = 0;
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Dashboard'),
+        title: const Text('PonselMart'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'Halaman Dashboard',
-          style: TextStyle(fontSize: 20),
+      bottomNavigationBar: const NavBot(),
+      body: SafeArea(
+        child: Row(
+          children: [
+            const SizedBox(),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      margin: EdgeInsets.only(
+                        left: 100,
+                        right: 100,
+                        top: 20,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Center(
+                            child: const Text(
+                              'Recomended',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 25,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
